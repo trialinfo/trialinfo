4f0615b3cc4b8c15f6aa65a774eb41cff35808a4
Andreas Gruenbacher <agruen@linbit.com>
2012-05-16 02:51:05 +0200

    Fehlende Lizenz-Header ergänzen

--- Wertungen.pm
+++ Wertungen.pm
@@ -1,3 +1,20 @@
+# Wertungen
+
+# Copyright (C) 2012  Andreas Gruenbacher  <andreas.gruenbacher@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You can find a copy of the GNU Affero General Public License at
+# <http://www.gnu.org/licenses/>.
+
 package Wertungen;
 
 require Exporter;
@@ -397,3 +414,5 @@ sub jahreswertung($$$$) {
     }
     doc_table ["Nr.", "Name"], $body, undef, ["r3", "l"];
 }
+
+1;
--- cgi-bin/statistik-ssi.pl
+++ cgi-bin/statistik-ssi.pl
@@ -1,5 +1,20 @@
 #! /usr/bin/perl -w -I..
 
+# Copyright (C) 2012  Andreas Gruenbacher  <andreas.gruenbacher@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You can find a copy of the GNU Affero General Public License at
+# <http://www.gnu.org/licenses/>.
+
 use CGI;
 use DBI;
 use RenderOutput;
