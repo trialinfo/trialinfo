678cea6423af6ace1109742127fed6099fdd633b
Andreas Gruenbacher <agruen@linbit.com>
2012-07-19 05:34:08 +0200

    Verbesserungen zu jquery.polartimer.js

--- htdocs/js/jquery.polartimer.js
+++ htdocs/js/jquery.polartimer.js
@@ -15,153 +15,155 @@
 
   var methods = {
     init : function(options) {
-      var state = {
-        timerSeconds : 10,
-        callback : function() {
-        },
-        color : '#CCC',
-        opacity : 1
-      };
-      state = $.extend(state, options);
+      var state = $.extend({
+	callback : function() { },
+	wait : 10000,
+	fill : '#CCC',
+	stroke : 'none',
+	opacity : 1.0
+      }, options);
 
       return this.each(function() {
-        var $this = $(this);
-        var data = $this.data('polartimer');
-        if (! data) {
-          $this.addClass('polartimer');
-          $this.height($this.width());
-          state.timer = null;
-          state.timerCurrent = 0;
-          state.pi2 = Math.PI * 2;
-          state.piOver2 = Math.PI / 2;
-          state.width = $this.width();
-          state.height = $this.height();
-          state.raphael = Raphael($this.context, state.width, state.height);
-          $this.data('polartimer', state);
-        }
+	var $this = $(this);
+	var data = $this.data('polartimer');
+	if (! data) {
+	  $this.addClass('polartimer');
+	  $this.height($this.width());
+	  state.timer = null;
+	  state.timerCurrent = 0;
+	  state.paper = Raphael($this.context, $this.width(), $this.height());
+	  $this.data('polartimer', state);
+	}
       });
     },
 
     stopWatch : function() {
       return this.each(function() {
-        var data = $(this).data('polartimer');
-        if (data) {
-          var seconds = (data.timerFinish - (new Date().getTime())) / 1000;
-          if(seconds <= 0) {
-            clearInterval(data.timer);
-            $(this).polartimer('drawTimer', 100);
-            data.callback();
-          }
-          else {
-            var percent = 100 - ((seconds / (data.timerSeconds)) * 100);
-            $(this).polartimer('drawTimer', percent);
-          }
-        }
+	var data = $(this).data('polartimer');
+	if (data) {
+	  var timeLeft = data.timerFinish - new Date().getTime();
+	  if(timeLeft <= 0) {
+	    clearInterval(data.timer);
+	    $(this).polartimer('drawTimer', 1.0);
+	    data.callback();
+	  }
+	  else {
+	    var f = 1 - timeLeft / data.wait;
+	    $(this).polartimer('drawTimer', f);
+	  }
+	}
       });
     },
 
-    drawTimer : function(percent) {
+    drawTimer : function(f) {
       return this.each(function() {
-        $this = $(this);
-        var data = $this.data('polartimer');
-        if (data) {
-          var w = data.width;
-          var h = data.height;
-          var cx = w / 2;
+	$this = $(this);
+	var data = $this.data('polartimer');
+	if (data) {
+	  var size = Math.min(data.paper.width, data.paper.height);
+	  var margin = 1;
+	  var radius = size / 2 - margin;
 
-          data.raphael.clear();
+	  data.paper.clear();
 
-          if (percent == 100) {
-            data.raphael.circle(cx, cx, cx).attr({
-              fill : data.color,
-              stroke : 'none',
-              opacity : data.opacity
-            });
-          }
-          else {
-            var theta = data.pi2 * percent / 100 - data.piOver2;
-            var x1 = Math.cos(theta) * cx + cx;
-            var y1 = Math.sin(theta) * cx + cx;
+	  var frame;
+	  if (f >= 1.0) {
+	    frame = data.paper.circle(radius, radius, radius);
+	  }
+	  else {
+	    var theta = 2 * Math.PI * (f - 0.25);
+	    var x1 = radius * (Math.cos(theta) + 1.0);
+	    var y1 = radius * (Math.sin(theta) + 1.0);
 
-            var longArcFlag = (percent <= 50) ? 0 : 1;
+	    var longArcFlag = (f <= 0.5) ? 0 : 1;
 
-            var path = "M" + cx + "," + cx + " L" + cx + ",0 ";
-            path += "A" + cx + "," + cx + " 0 " + longArcFlag + ",1 " + x1 + "," + y1 + " ";
-            path += "L" + cx + "," + cx + "z";
+	    var path = "M" + radius + "," + radius + " L" + radius + ",0 " +
+		       "A" + radius + "," + radius + " 0 " + longArcFlag + ",1 " +
+			     x1 + "," + y1 + " " +
+		       "L" + radius + "," + radius + "z";
 
-            var frame = data.raphael.path(path);
-            frame.attr({
-              fill : data.color,
-              stroke : 'none',
-              opacity : data.opacity
-            });
-          }
-        }
+	    frame = data.paper.path(path);
+	  }
+	  frame.translate(margin, margin);
+	  frame.attr({
+	    fill : data.fill,
+	    stroke : data.stroke,
+	    opacity : data.opacity
+	  });
+	}
       });
     },
 
-    start : function() {
+    start : function(wait, callback) {
       return this.each(function() {
-        var data = $(this).data('polartimer');
-        if (data) {
-          clearInterval(data.timer);
-          data.resumeSeconds = null; // clears paused state
-          data.timerFinish = new Date().getTime() + (data.timerSeconds * 1000);
-          $(this).polartimer('drawTimer', 0);
-          var id = $this.attr('id');
-          data.timer = (! id || id === "") ?
-            setInterval("$this.polartimer('stopWatch')", 50) :
-            setInterval("$('#"+id+"').polartimer('stopWatch')", 50);
-        }
+	var data = $(this).data('polartimer');
+	if (data) {
+	  if (typeof(wait) != 'undefined') {
+	    data.wait = wait;
+	  }
+	  if (typeof(callback) !== 'undefined') {
+	    data.callback = callback;
+	  }
+	  clearInterval(data.timer);
+	  data.resume = null; // clears paused state
+	  data.timerFinish = new Date().getTime() + data.wait;
+	  $(this).polartimer('drawTimer', 0.0);
+	  var id = $this.attr('id');
+	  data.timer = (! id || id === "") ?
+	    setInterval("$this.polartimer('stopWatch')", 40) :
+	    setInterval("$('#"+id+"').polartimer('stopWatch')", 40);
+	  /* FIXME: Why not $(this).setInterval()? */
+	}
       });
     },
 
     pause : function() {
       return this.each(function() {
-        var data = $(this).data('polartimer');
-        if (data && ! data.resumeSeconds) {
-          data.resumeSeconds = (data.timerFinish - (new Date().getTime())) / 1000;
-          clearInterval(data.timer);
-        }
+	var data = $(this).data('polartimer');
+	if (data && ! data.resume) {
+	  data.resume = data.timerFinish - new Date().getTime();
+	  clearInterval(data.timer);
+	}
       });
     },
 
     resume : function() {
       return this.each(function() {
-        var data = $(this).data('polartimer');
-        if (data && data.resumeSeconds) {
-          clearInterval(data.timer);
-          data.timerFinish = new Date().getTime() + (data.resumeSeconds * 1000);
-          data.resumeSeconds = null;
-          $(this).polartimer('drawTimer', 0);
-          var id = $this.attr('id');
-          data.timer = (! id || id === "") ?
-            setInterval("$this.polartimer('stopWatch')", 50) :
-            setInterval("$('#"+id+"').polartimer('stopWatch')", 50);
-        }
+	var data = $(this).data('polartimer');
+	if (data && data.resume) {
+	  clearInterval(data.timer);
+	  data.timerFinish = new Date().getTime() + data.resume;
+	  data.resume = null;
+	  $(this).polartimer('drawTimer', 0.0);
+	  var id = $this.attr('id');
+	  data.timer = (! id || id === "") ?
+	    setInterval("$this.polartimer('stopWatch')", 40) :
+	    setInterval("$('#"+id+"').polartimer('stopWatch')", 40);
+	  /* FIXME: Why not $(this).setInterval()? */
+	}
       });
     },
 
     reset : function() {
       return this.each(function() {
-        var data = $(this).data('polartimer');
-        if (data) {
-          clearInterval(data.timer);
-          data.resumeSeconds = null; // clears paused state
-          $(this).polartimer('drawTimer', 0);
-        }
+	var data = $(this).data('polartimer');
+	if (data) {
+	  clearInterval(data.timer);
+	  data.resume = null; // clears paused state
+	  $(this).polartimer('drawTimer', 0.0);
+	}
       });
     },
 
     destroy : function() {
       return this.each(function() {
-        var $this = $(this);
-        var data = $this.data('polartimer');
-        if (data) {
-          clearInterval(data.timer);
-          data.raphael.remove();
-          $this.removeData('polartimer');
-        }
+	var $this = $(this);
+	var data = $this.data('polartimer');
+	if (data) {
+	  clearInterval(data.timer);
+	  data.paper.remove();
+	  $this.removeData('polartimer');
+	}
       });
     }
 
