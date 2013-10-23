7b4936bf1a85c8171b890a4a28e2ae47c7b1e72f
Andreas Gruenbacher <agruen@linbit.com>
2013-10-24 00:14:32 +0200

    Timestamp formatieren und parsen

--- htdocs/admin/directives.js
+++ htdocs/admin/directives.js
@@ -1,79 +1,137 @@
 'use strict;'
 
-// Model: yyyy-MM-dd, View: dd.MM.yyyy
+function parse_iso_date($scope, text) {
+  if (typeof text == 'string') {
+    var match;
+    if (text == '')
+      return null;
+    else if (match = text.match(/^(\d{1,2})\.(\d{1,2})\.(\d{2}|\d{4})$/)) {
+      if (match[3] <= 99)
+	match[3] = +match[3] + 100 * (19 + (match[3] <= (new Date()).getYear() % 100));
+      var date = new Date(match[3], match[2] - 1, match[1]);
+      if (date.getFullYear() == match[3] &&
+	  date.getMonth() == match[2] - 1 &&
+	  date.getDate() == match[1])
+	return $scope.$eval('date | date:"yyyy-MM-dd"', {date: date});
+    }
+  }
+}
+
+function format_iso_date($scope, value, format) {
+  var match;
+  if (value == null)
+    return '';
+  if (typeof value == 'string' &&
+      (match = value.match(/^(\d{4})-(\d{2})-(\d{2})$/)))
+    return $scope.$eval('date | date:"' + format + '"',
+		        {date: new Date(match[1], match[2] - 1, match[3])});
+  else
+    return value;
+}
+
+// Model: yyyy-MM-dd, View: d.M.yyyy
 function isoDateDirective() {
   return {
     restrict: 'A',
     require: 'ngModel',
     link: function(scope, element, attr, ctrl) {
       ctrl.$parsers.push(function(text) {
-	var match;
-	if (typeof text == 'string') {
-	  if (text == '') {
-	    ctrl.$setValidity('isoDate', true);
-	    return null;
-	  } else if (match = text.match(/^(\d{1,2})\.(\d{1,2})\.(\d{2}|\d{4})$/)) {
-	    if (match[3] <= 99)
-	      match[3] = +match[3] + 100 * (19 + (match[3] <= (new Date()).getYear() % 100));
-	    var date = new Date(match[3], match[2] - 1, match[1]);
-	    if (date.getFullYear() == match[3] &&
-		date.getMonth() == match[2] - 1 &&
-		date.getDate() == match[1]) {
-	      ctrl.$setValidity('isoDate', true);
-	      return ('00' + date.getFullYear()).slice(-4) + '-' +
-		     ('0' + (date.getMonth() + 1)).slice(-2) + '-' +
-		     ('0' + date.getDate()).slice(-2);
-	    }
-	  } else
-	    ctrl.$setValidity('isoDate', false);
-	}
+	var value = parse_iso_date(scope, text);
+	ctrl.$setValidity('isoDate', value !== undefined);
+	return value;
       });
       ctrl.$formatters.push(function(value) {
-	var match;
-	if (value == null)
-	  return '';
-	if (typeof value == 'string' &&
-	    (match = value.match(/^(\d{4})-(\d{2})-(\d{2})$/)))
-	  return +match[3] + '.' + +match[2] + '.' + +match[1];
-	else
-	  return value;
+	return format_iso_date(scope, value, 'd.M.yyyy');
       });
     }
   };
 }
 
-// Model: HH:mm:ss, View: HH:mm
+function parse_iso_time($scope, text) {
+  if (typeof text == 'string') {
+    var match;
+    if (text == '')
+      return null;
+    else if (match = text.match(/^(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?$/)) {
+      var time = new Date(0, 0, 0, match[1], match[2], match[3] || 0);
+      if (time.getHours() == match[1] &&
+	  time.getMinutes() == match[2])
+	return $scope.$eval('time | date:"HH:mm:ss"', {time: time});
+    }
+  }
+}
+
+function format_iso_time($scope, value, format) {
+  if (value == null)
+    return '';
+  if (typeof value == 'string' &&
+      (match = value.match(/^(\d{2}):(\d{2}):(\d{2})$/)))
+    return $scope.$eval('time | date:"' + format + '"',
+		        {time: new Date(0, 0, 0, match[1], match[2], match[3])});
+  else
+    return value;
+}
+
+// Model: HH:mm:ss, View: H:mm
 function isoTimeDirective() {
   return {
     restrict: 'A',
     require: 'ngModel',
     link: function(scope, element, attr, ctrl) {
       ctrl.$parsers.push(function(text) {
-	var match;
-	if (typeof text == 'string') {
-	  if (text == '') {
-	    ctrl.$setValidity('isoTime', true);
-	    return null;
-	  } else if (match = text.match(/^(\d{1,2}):(\d{2})$/)) {
-	    var date = new Date(0, 0, 0, match[1], match[2]);
-	    if (date.getHours() == match[1] &&
-		date.getMinutes() == match[2]) {
-	      ctrl.$setValidity('isoTime', true);
-	      return ('0' + date.getHours()).slice(-2) + ':' +
-		     ('0' + date.getMinutes()).slice(-2) + ':00';
-	    }
-	  } else
-	    ctrl.$setValidity('isoTime', false);
-	}
+	var value = parse_iso_time(scope, text);
+	ctrl.$setValidity('isoTime', value !== undefined);
+	return value;
       });
       ctrl.$formatters.push(function(value) {
-	if (value == null)
-	  return '';
-	if (typeof value == 'string' &&
-	    (match = value.match(/^(\d{2}):(\d{2}):(\d{2})$/)))
-	  return +match[1] + ':' + match[2];
-	else
-	  return value;
+	return format_iso_time(scope, value, 'H:mm');
+      });
+    }
+  };
+}
+
+function format_iso_timestamp($scope, value) {
+  if (value == null)
+    return '';
+  else if (typeof value == 'string') {
+    var split = value.split(/ /);
+    if (split.length == 2) {
+      split[0] = format_iso_date($scope, split[0], 'd.M.yyyy');
+      split[1] = format_iso_time($scope, split[1], 'H:mm:ss');
+      if (split[0] !== undefined && split[1] !== undefined)
+	return split[0] + ' ' + split[1];
+    }
+  }
+  return value;
+}
+
+function parse_iso_timestamp(text) {
+  if (typeof text == 'string' && text == '')
+    return null;
+  else {
+    var split = text.split(/ /);
+    if (split.length == 2) {
+      split[0] = parse_iso_date(scope, split[0]);
+      split[1] = parse_iso_time(scope, split[1]);
+      if (split[0] !== undefined && split[1] !== undefined)
+	return split[0] + ' ' + split[1];
+    }
+  }
+}
+
+// Model: yyyy-MM-dd HH:mm:ss, View: d.M.yyyy H:mm:ss
+function isoTimestampDirective() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attr, ctrl) {
+      ctrl.$parsers.push(function(text) {
+	var value = parse_iso_timestamp(text);
+	ctrl.$setValidity('isoTime', value !== undefined);
+	return value;
+      });
+      ctrl.$formatters.push(function(value) {
+	return format_iso_timestamp(scope, value);
       });
     }
   };
