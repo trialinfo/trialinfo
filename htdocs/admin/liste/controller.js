4f21c2c8ec7734ec8d841783b4a9848f669cbeaa
Andreas Gruenbacher <agruen@linbit.com>
2014-01-18 14:09:45 +0100

    Fahrerliste: Anzeigeoptionen in URL

--- htdocs/admin/liste/controller.js
+++ htdocs/admin/liste/controller.js
@@ -1,9 +1,10 @@
 'use strict;'
 
-function listeController($scope, veranstaltung, fahrerliste) {
+function listeController($scope, $route, $location, veranstaltung, fahrerliste) {
   $scope.veranstaltung = veranstaltung;
   $scope.features = features_aus_liste(veranstaltung);
   $scope.fold = {};
+  $scope.anzeige = { felder: [] };
 
   var versicherungen = {
     1: 'ADAC-Versicherung',
@@ -257,6 +258,94 @@ function listeController($scope, veranstaltung, fahrerliste) {
       return gruppieren.heading(gruppe[0]);
   };
 
+  var tristate_optionen = (function() {
+    var felder = ['startnummer', 'nennungseingang', 'papierabnahme'];
+    for (var n = 1; n <= 4; n++)
+      felder.push('wertung' + n);
+    return felder;
+  })();
+
+  function startende_klassen() {
+    var klassen = {};
+    angular.forEach(veranstaltung.sektionen, function(sektionen, index) {
+      if (sektionen && sektionen.length)
+	klassen[index + 1] = true;
+    });
+    return klassen;
+  }
+
+  function von_url(anzeige) {
+    angular.forEach(tristate_optionen, function(option) {
+      if (anzeige[option] === 'yes')
+	anzeige[option] = true;
+      else if (anzeige[option] === 'no')
+	anzeige[option] = false;
+      else
+	anzeige[option] = null;
+    });
+    angular.forEach(['min', 'max'], function(option) {
+      if (anzeige[option] === undefined)
+	anzeige[option] = null;
+      else
+        anzeige[option] = +anzeige[option];
+    });
+
+    var klassen = startende_klassen();
+    klassen['-'] = true;
+    angular.forEach(anzeige.klasse, function(klasse) {
+      klassen[klasse] = false;
+    });
+    anzeige.klassen = klassen;
+    delete anzeige.klasse;
+
+    var felder = anzeige.feld || [];
+    if (typeof felder === 'string')
+      felder = [felder];
+    if (felder.length == 0 || felder[felder.length - 1] !== '')
+      felder.push('');
+    anzeige.felder = felder;
+    delete anzeige.feld;
+
+    return anzeige;
+  }
+
+  function nach_url() {
+    var anzeige = angular.copy($scope.anzeige);
+
+    angular.forEach(tristate_optionen, function(option) {
+      if (anzeige[option] !== null)
+	anzeige[option] = anzeige[option] ? 'yes' : 'no';
+    });
+
+    var versteckte_klassen = [];
+    angular.forEach(anzeige.klassen, function(value, key) {
+      if (value === false)
+	versteckte_klassen.push(key);
+    });
+    if (versteckte_klassen.length)
+      anzeige.klasse = versteckte_klassen;
+    delete anzeige.klassen;
+
+    var felder = anzeige.felder;
+    if (felder[felder.length - 1] === '')
+      felder.pop();
+    anzeige.feld = felder;
+    delete anzeige.felder;
+
+    angular.forEach(anzeige, function(value, key) {
+      if (value === null || value === '')
+	delete anzeige[key];
+    });
+    return anzeige;
+  }
+
+  function url_aktualisieren() {
+    var url_alt = $location.search();
+    var url_neu = nach_url();
+    if (!angular.equals(url_alt, url_neu))
+      $location.search(url_neu).replace();
+  }
+
   function aktualisieren() {
     var ergebnisliste = [];
     angular.forEach(fahrerliste, function(fahrer) {
@@ -274,38 +363,13 @@ function listeController($scope, veranstaltung, fahrerliste) {
     $scope.ergebnisliste = gruppieren ?
       group_by(ergebnisliste, gruppieren.compare) :
       [ergebnisliste];
+    url_aktualisieren();
   };
 
   $scope.nur_wenn_positiv = function(x) {
     if (x > 0)
       return x;
   };
-
-  $scope.anzeige = {
-    startnummer: true,
-    nennungseingang: null,
-    papierabnahme: true,
-    wertung1: null,
-    wertung2: null,
-    wertung3: null,
-    wertung4: null,
-    gruppierung: 'klasse',
-    reihenfolge: 'startnummer',
-    klassen: function() {
-      var klassen = { '-': true };
-      angular.forEach(veranstaltung.sektionen, function(sektionen, index) {
-	if (sektionen && sektionen.length)
-	  klassen[index + 1] = true;
-      });
-      return klassen;
-    }(),
-    felder: [
-      'startnummer',
-      'klasse',
-      'name',
-    ]
-  };
-  $scope.felder = [];
 
   $scope.$watch('anzeige.startnummer', function() {
     if ($scope.anzeige.startnummer !== true) {
@@ -329,6 +393,22 @@ function listeController($scope, veranstaltung, fahrerliste) {
 	$scope.felder.push(feld);
     }
   }, true);
+
+  $scope.$on('$routeUpdate', function() {
+    var search = $location.search();
+    if (angular.equals(search, {})) {
+      search = {
+	startnummer: 'yes',
+	papierabnahme: 'yes',
+	gruppierung: 'klasse',
+	reihenfolge: 'startnummer',
+	klasse: ['-'],
+	feld: ['startnummer', 'name']
+      };
+    }
+    angular.extend($scope.anzeige, von_url(search));
+  });
+  $scope.$emit('$routeUpdate');
 }
 
 listeController.resolve = {
