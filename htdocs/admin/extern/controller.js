b58f146d542b200f73831d5baf0650a181bee61d
Andreas Gruenbacher <agruen@linbit.com>
2014-04-20 17:41:43 +0200

    Fehlerbehandlung hat hier gefehlt

--- htdocs/admin/extern/controller.js
+++ htdocs/admin/extern/controller.js
@@ -50,11 +50,11 @@ function externController($scope, $http, $location, $q, veranstaltungen) {
 	    $http.post('/api/trialtool/import', data).
 	      success(function(result) {
 		$location.path('/veranstaltung/' + result.id).replace();
-	    }).
-	    error(netzwerkfehler).
-	    finally(function() {
-	      $scope.busy = false;
-	    });
+	      }).
+	      error(netzwerkfehler).
+	      finally(function() {
+		$scope.busy = false;
+	      });
 	  }
 	}
 
@@ -147,6 +147,7 @@ function externController($scope, $http, $location, $q, veranstaltungen) {
 	  success(function(result) {
 	    $location.path('/veranstaltung/' + result.id).replace();
 	  }).
+	  error(netzwerkfehler).
 	  finally(function() {
 	    $scope.busy = false;
 	    cancel_remote = undefined;
