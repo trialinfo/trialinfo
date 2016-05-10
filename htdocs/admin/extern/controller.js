01859cc8d6e2ddb1b88ea25b55f03735c4163055
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2016-05-10 02:59:57 +0200

    Minifizierbar machen, minifiziertes Angular verwenden

--- htdocs/admin/extern/controller.js
+++ htdocs/admin/extern/controller.js
@@ -1,187 +1,191 @@
 'use strict;'
 
-function externController($scope, $http, $location, $q, veranstaltungen) {
-  $scope.$root.kontext((SYNC_SOURCE ? 'Synchronisieren, ' : '') + 'Import und Export');
-  $scope.SYNC_SOURCE = SYNC_SOURCE;
-  $scope.veranstaltungen = veranstaltungen;
-  $scope.einstellungen = {
-    operation: $scope.SYNC_SOURCE ? 'sync' : 'export-file',
-    format: 'trial-auswertung',
-    timeout: 30,
-    url: 'https://otsv.trialinfo.at'
-  };
-  try {
-    $scope.einstellungen.veranstaltung = veranstaltungen[veranstaltungen.length - 1];
-  } catch(_) { }
-  $scope.remote = {};
+var externController = [
+  '$scope', '$http', '$location', '$q', 'veranstaltungen',
+  function ($scope, $http, $location, $q, veranstaltungen) {
+    $scope.$root.kontext((SYNC_SOURCE ? 'Synchronisieren, ' : '') + 'Import und Export');
+    $scope.SYNC_SOURCE = SYNC_SOURCE;
+    $scope.veranstaltungen = veranstaltungen;
+    $scope.einstellungen = {
+      operation: $scope.SYNC_SOURCE ? 'sync' : 'export-file',
+      format: 'trial-auswertung',
+      timeout: 30,
+      url: 'https://otsv.trialinfo.at'
+    };
+    try {
+      $scope.einstellungen.veranstaltung = veranstaltungen[veranstaltungen.length - 1];
+    } catch(_) { }
+    $scope.remote = {};
 
-  $scope.import_file = function() {
-    if ($scope.einstellungen.format == 'trial-auswertung') {
-      var tra_datei = document.getElementById('tra_datei');
-      if (tra_datei && tra_datei.files[0]) {
-	var reader = new FileReader();
-	reader.onloadend = function(e) {
-	  var data = e.target.result;
-	  $http.post('/api/veranstaltung/import', window.btoa(data)).
-	    success(function(result) {
-	      if (result.id != null)
-		$location.path('/veranstaltung/' + result.id).replace();
-	    }).
-	    error(netzwerkfehler).
-	    finally(function() {
-	      $scope.busy = false;
-	    });
-	};
-	$scope.busy = true;
-	reader.readAsBinaryString(tra_datei.files[0]);
-      }
-    }
-    if ($scope.einstellungen.format == 'trialtool') {
-      var cfg_datei = document.getElementById('cfg_datei');
-      var dat_datei = document.getElementById('dat_datei');
-      if (cfg_datei && cfg_datei.files[0] && dat_datei && dat_datei.files[0]) {
-	var cfg_data, dat_data;
-
-	function datei_gelesen() {
-	  if (cfg_data != null && dat_data != null) {
-	    var data = angular.toJson({ cfg: window.btoa(cfg_data), dat: window.btoa(dat_data) });
-	    $http.post('/api/trialtool/import', data).
+    $scope.import_file = function() {
+      if ($scope.einstellungen.format == 'trial-auswertung') {
+	var tra_datei = document.getElementById('tra_datei');
+	if (tra_datei && tra_datei.files[0]) {
+	  var reader = new FileReader();
+	  reader.onloadend = function(e) {
+	    var data = e.target.result;
+	    $http.post('/api/veranstaltung/import', window.btoa(data)).
 	      success(function(result) {
-		$location.path('/veranstaltung/' + result.id).replace();
+		if (result.id != null)
+		  $location.path('/veranstaltung/' + result.id).replace();
 	      }).
 	      error(netzwerkfehler).
 	      finally(function() {
 		$scope.busy = false;
 	      });
-	  }
+	  };
+	  $scope.busy = true;
+	  reader.readAsBinaryString(tra_datei.files[0]);
 	}
+      }
+      if ($scope.einstellungen.format == 'trialtool') {
+	var cfg_datei = document.getElementById('cfg_datei');
+	var dat_datei = document.getElementById('dat_datei');
+	if (cfg_datei && cfg_datei.files[0] && dat_datei && dat_datei.files[0]) {
+	  var cfg_data, dat_data;
 
-	var cfg_reader = new FileReader();
-	cfg_reader.onloadend = function(e) {
-	  cfg_data = e.target.result;
-	  datei_gelesen();
-	};
-	var dat_reader = new FileReader();
-	dat_reader.onloadend = function(e) {
-	  dat_data = e.target.result;
-	  datei_gelesen();
-	};
-	$scope.busy = true;
-	cfg_reader.readAsBinaryString(cfg_datei.files[0]);
-	dat_reader.readAsBinaryString(dat_datei.files[0]);
+	  function datei_gelesen() {
+	    if (cfg_data != null && dat_data != null) {
+	      var data = angular.toJson({ cfg: window.btoa(cfg_data), dat: window.btoa(dat_data) });
+	      $http.post('/api/trialtool/import', data).
+		success(function(result) {
+		  $location.path('/veranstaltung/' + result.id).replace();
+		}).
+		error(netzwerkfehler).
+		finally(function() {
+		  $scope.busy = false;
+		});
+	    }
+	  }
+
+	  var cfg_reader = new FileReader();
+	  cfg_reader.onloadend = function(e) {
+	    cfg_data = e.target.result;
+	    datei_gelesen();
+	  };
+	  var dat_reader = new FileReader();
+	  dat_reader.onloadend = function(e) {
+	    dat_data = e.target.result;
+	    datei_gelesen();
+	  };
+	  $scope.busy = true;
+	  cfg_reader.readAsBinaryString(cfg_datei.files[0]);
+	  dat_reader.readAsBinaryString(dat_datei.files[0]);
+	}
       }
     }
-  }
 
-  $scope.dateiname = function(veranstaltung) {
-    if (veranstaltung.dateiname != null)
-      return veranstaltung.dateiname;
-    var titel = veranstaltung.titel;
-    if (titel) {
-      titel = titel.replace(/[:\/\\]/g, '')
-      if (titel)
-	return titel
-    }
-    if (veranstaltung.datum != null)
-      return 'Trial ' + veranstaltung.datum;
-    else
-      return 'Trial';
-  };
-
-  $scope.synchronize = function() {
-    var args = {
-      titel: $scope.einstellungen.veranstaltung.titel,
-      tag: $scope.einstellungen.veranstaltung.tag,
-      url: $scope.einstellungen.url,
-      timeout: $scope.einstellungen.timeout * 1000
+    $scope.dateiname = function(veranstaltung) {
+      if (veranstaltung.dateiname != null)
+	return veranstaltung.dateiname;
+      var titel = veranstaltung.titel;
+      if (titel) {
+	titel = titel.replace(/[:\/\\]/g, '')
+	if (titel)
+	  return titel
+      }
+      if (veranstaltung.datum != null)
+	return 'Trial ' + veranstaltung.datum;
+      else
+	return 'Trial';
     };
-    $scope.$root.$broadcast('sync', args);
-  }
 
-  var cancel_remote;
-  $scope.get_veranstaltungen = function() {
-    $scope.busy = true;
-    cancel_remote = $q.defer();
-    $http.get($scope.einstellungen.url + '/api/veranstaltungen',
-	      {timeout: cancel_remote.promise, withCredentials: true}).
-      success(function(veranstaltungen) {
-	$scope.remote.veranstaltungen = veranstaltungen;
-	$scope.remote.veranstaltung =
-	  veranstaltungen.length ? veranstaltungen[veranstaltungen.length - 1] : null;
-	$scope.liste_abgerufen = true;
-      }).
-      error(netzwerkfehler).
-      finally(function() {
-	$scope.busy = false;
-	cancel_remote = undefined;
-      });
-  };
-
-  $scope.veranstaltung_bezeichnung = veranstaltung_bezeichnung;
-
-  $scope.$watch('remote.veranstaltung', function() {
-    var exists = false;
-    if ($scope.remote.veranstaltung) {
-      angular.forEach(veranstaltungen, function(veranstaltung) {
-	if (veranstaltung.tag == $scope.remote.veranstaltung.tag)
-	  exists = true;
-      });
+    $scope.synchronize = function() {
+      var args = {
+	titel: $scope.einstellungen.veranstaltung.titel,
+	tag: $scope.einstellungen.veranstaltung.tag,
+	url: $scope.einstellungen.url,
+	timeout: $scope.einstellungen.timeout * 1000
+      };
+      $scope.$root.$broadcast('sync', args);
     }
-    $scope.remote.exists = exists;
-  });
 
-  $scope.import_remote = function() {
-    $scope.busy = true;
-    var tag = $scope.remote.veranstaltung.tag;
-    if (cancel_remote)
-      cancel_remote.resolve();
-    cancel_remote = $q.defer();
-    $http.get($scope.einstellungen.url + '/api/veranstaltung/export',
-	      {params: {tag: tag}, timeout: cancel_remote.promise, withCredentials: true, responseType: 'arraybuffer'}).
-      success(function(data) {
-	var params;
-	if (!$scope.remote.exists) {
-	  params = {
-	    tag: tag,
-	    create: true,  // Veranstaltung darf noch nicht existieren
-	  };
-	} else if ($scope.remote.replace) {
-	  params = {
-	    tag: tag,
-	  };
-	}
-	var enc = window.btoa(String.fromCharCode.apply(null, new Uint8Array(data)));
-	cancel_remote = $q.defer();
-	$http.post('/api/veranstaltung/import', enc,
-		   {params: params, timeout: cancel_remote.promise}).
-	  success(function(result) {
-	    $location.path('/veranstaltung/' + result.id).replace();
-	  }).
-	  error(netzwerkfehler).
-	  finally(function() {
-	    $scope.busy = false;
-	    cancel_remote = undefined;
-	  });
-      }).
-      error(function() {
-	$scope.busy = false;
-	cancel_remote = undefined;
-	netzwerkfehler();
-      });
-  };
+    var cancel_remote;
+    $scope.get_veranstaltungen = function() {
+      $scope.busy = true;
+      cancel_remote = $q.defer();
+      $http.get($scope.einstellungen.url + '/api/veranstaltungen',
+		{timeout: cancel_remote.promise, withCredentials: true}).
+	success(function(veranstaltungen) {
+	  $scope.remote.veranstaltungen = veranstaltungen;
+	  $scope.remote.veranstaltung =
+	    veranstaltungen.length ? veranstaltungen[veranstaltungen.length - 1] : null;
+	  $scope.liste_abgerufen = true;
+	}).
+	error(netzwerkfehler).
+	finally(function() {
+	  $scope.busy = false;
+	  cancel_remote = undefined;
+	});
+    };
 
-  $scope.$watch('einstellungen.url', function() {
-    $scope.liste_abgerufen = false;
-  });
+    $scope.veranstaltung_bezeichnung = veranstaltung_bezeichnung;
 
-  $scope.$on('$destroy', function() {
-    if (cancel_remote)
-      cancel_remote.resolve();
-  });
-}
+    $scope.$watch('remote.veranstaltung', function() {
+      var exists = false;
+      if ($scope.remote.veranstaltung) {
+	angular.forEach(veranstaltungen, function(veranstaltung) {
+	  if (veranstaltung.tag == $scope.remote.veranstaltung.tag)
+	    exists = true;
+	});
+      }
+      $scope.remote.exists = exists;
+    });
+
+    $scope.import_remote = function() {
+      $scope.busy = true;
+      var tag = $scope.remote.veranstaltung.tag;
+      if (cancel_remote)
+	cancel_remote.resolve();
+      cancel_remote = $q.defer();
+      $http.get($scope.einstellungen.url + '/api/veranstaltung/export',
+		{params: {tag: tag}, timeout: cancel_remote.promise, withCredentials: true, responseType: 'arraybuffer'}).
+	success(function(data) {
+	  var params;
+	  if (!$scope.remote.exists) {
+	    params = {
+	      tag: tag,
+	      create: true,  // Veranstaltung darf noch nicht existieren
+	    };
+	  } else if ($scope.remote.replace) {
+	    params = {
+	      tag: tag,
+	    };
+	  }
+	  var enc = window.btoa(String.fromCharCode.apply(null, new Uint8Array(data)));
+	  cancel_remote = $q.defer();
+	  $http.post('/api/veranstaltung/import', enc,
+		     {params: params, timeout: cancel_remote.promise}).
+	    success(function(result) {
+	      $location.path('/veranstaltung/' + result.id).replace();
+	    }).
+	    error(netzwerkfehler).
+	    finally(function() {
+	      $scope.busy = false;
+	      cancel_remote = undefined;
+	    });
+	}).
+	error(function() {
+	  $scope.busy = false;
+	  cancel_remote = undefined;
+	  netzwerkfehler();
+	});
+    };
+
+    $scope.$watch('einstellungen.url', function() {
+      $scope.liste_abgerufen = false;
+    });
+
+    $scope.$on('$destroy', function() {
+      if (cancel_remote)
+	cancel_remote.resolve();
+    });
+  }];
 
 externController.resolve = {
-  veranstaltungen: function($q, $http) {
-    return http_request($q, $http.get('/api/veranstaltungen'));
-  },
+  veranstaltungen: [
+    '$q', '$http',
+    function($q, $http) {
+      return http_request($q, $http.get('/api/veranstaltungen'));
+    }],
 };
--- htdocs/admin/sektionen/controller.js
+++ htdocs/admin/sektionen/controller.js
@@ -1,145 +1,149 @@
 'use strict;'
 
-function sektionenController($scope, $http, $timeout, veranstaltung) {
-  $scope.$root.kontext(veranstaltung.wertungen[0].titel);
+var sektionenController = [
+  '$scope', '$http', '$timeout', 'veranstaltung',
+  function ($scope, $http, $timeout, veranstaltung) {
+    $scope.$root.kontext(veranstaltung.wertungen[0].titel);
 
-  $scope.startende_klassen = function() {
-    var startende_klassen = {};
-    for (var klasse = 1; klasse <= veranstaltung.klassen.length; klasse++) {
-      var wertungsklasse = veranstaltung.klassen[klasse - 1].wertungsklasse;
-      if (veranstaltung.sektionen[wertungsklasse - 1])
-	startende_klassen[wertungsklasse] = true;
+    $scope.startende_klassen = function() {
+      var startende_klassen = {};
+      for (var klasse = 1; klasse <= veranstaltung.klassen.length; klasse++) {
+	var wertungsklasse = veranstaltung.klassen[klasse - 1].wertungsklasse;
+	if (veranstaltung.sektionen[wertungsklasse - 1])
+	  startende_klassen[wertungsklasse] = true;
+      }
+      return Object.keys(startende_klassen).sort(function(a, b) { return a - b; });
+    }();
+    veranstaltung_zuweisen(veranstaltung);
+
+    $scope.runden = function(klasse) {
+      var runden = [];
+      for (var runde = 1; runde <= veranstaltung.klassen[klasse - 1].runden; runde++)
+	runden.push(runde);
+      return runden;
+    };
+
+    function veranstaltung_zuweisen(_veranstaltung) {
+      if (_veranstaltung === undefined)
+	$scope.sektion_aus_wertung = angular.copy($scope.sektion_aus_wertung_alt);
+      else {
+	veranstaltung = _veranstaltung;
+	$scope.veranstaltung = veranstaltung;
+	$scope.veranstaltung_alt = angular.copy(veranstaltung);
+	$scope.sektion_aus_wertung = sektion_aus_wertung(veranstaltung);
+	$scope.sektion_aus_wertung_alt = angular.copy($scope.sektion_aus_wertung);
+      }
     }
-    return Object.keys(startende_klassen).sort(function(a, b) { return a - b; });
-  }();
-  veranstaltung_zuweisen(veranstaltung);
 
-  $scope.runden = function(klasse) {
-    var runden = [];
-    for (var runde = 1; runde <= veranstaltung.klassen[klasse - 1].runden; runde++)
-      runden.push(runde);
-    return runden;
-  };
+    function sektion_aus_wertung(veranstaltung) {
+      var sektionen_aus_wertung = veranstaltung.sektionen_aus_wertung;
+      var s = [];
+      angular.forEach($scope.startende_klassen, function(klasse) {
+	s[klasse - 1] = [];
+	for (var runde = 1; runde <= veranstaltung.klassen[klasse - 1].runden; runde++) {
+	  s[klasse - 1][runde - 1] = [];
+	  angular.forEach(veranstaltung.sektionen[klasse - 1], function(sektion) {
+	    s[klasse - 1][runde - 1][sektion - 1] = false;
+	  });
+	}
+      });
 
-  function veranstaltung_zuweisen(_veranstaltung) {
-    if (_veranstaltung === undefined)
-      $scope.sektion_aus_wertung = angular.copy($scope.sektion_aus_wertung_alt);
-    else {
-      veranstaltung = _veranstaltung;
-      $scope.veranstaltung = veranstaltung;
-      $scope.veranstaltung_alt = angular.copy(veranstaltung);
-      $scope.sektion_aus_wertung = sektion_aus_wertung(veranstaltung);
-      $scope.sektion_aus_wertung_alt = angular.copy($scope.sektion_aus_wertung);
+      angular.forEach(veranstaltung.sektionen_aus_wertung, function(sektionen, index) {
+	var klasse = index + 1;
+	if (sektionen && s[klasse - 1])
+	  angular.forEach(sektionen, function(sektionen, index) {
+	    var runde = index + 1;
+	    if (sektionen && s[klasse - 1][runde - 1])
+	      angular.forEach(sektionen, function(sektion) {
+		s[klasse - 1][runde - 1][sektion - 1] = true;
+	      });
+	  });
+      });
+      return s;
     }
-  }
 
-  function sektion_aus_wertung(veranstaltung) {
-    var sektionen_aus_wertung = veranstaltung.sektionen_aus_wertung;
-    var s = [];
-    angular.forEach($scope.startende_klassen, function(klasse) {
-      s[klasse - 1] = [];
-      for (var runde = 1; runde <= veranstaltung.klassen[klasse - 1].runden; runde++) {
-	s[klasse - 1][runde - 1] = [];
-	angular.forEach(veranstaltung.sektionen[klasse - 1], function(sektion) {
-	  s[klasse - 1][runde - 1][sektion - 1] = false;
+    function sektionen_aus_wertung(sektion_aus_wertung) {
+      var k = [];
+      angular.forEach(sektion_aus_wertung, function(sektionen, index) {
+	var klasse = index + 1;
+	if (sektionen) {
+	  var r = [];
+	  angular.forEach(sektionen, function(sektionen, index) {
+	    var runde = index + 1;
+	    if (sektionen) {
+	      var s = [];
+	      angular.forEach(sektionen, function(sektion, index) {
+		if (sektion)
+		  s.push(index + 1);
+	      });
+	      if (s.length)
+		r[runde - 1] = s;
+	    }
+	  });
+	  if (r.length)
+	    k[klasse - 1] = r;
+	}
+      });
+      return k;
+    }
+
+    $scope.geaendert = function() {
+      return !(angular.equals($scope.sektion_aus_wertung, $scope.sektion_aus_wertung_alt) &&
+	       angular.equals($scope.veranstaltung, $scope.veranstaltung_alt));
+    };
+
+    $scope.speichern = function() {
+      if ($scope.busy)
+	return;
+      /* FIXME: Wenn Klasse schon Starter hat, muss sie weiterhin starten. (Verweis auf Starterliste.) */
+      veranstaltung.sektionen_aus_wertung = sektionen_aus_wertung($scope.sektion_aus_wertung);
+
+      /* Wenn die Daten aus dem Trialtool importiert wurden, ist das Feature
+	 sektionen_aus_wertung nicht gesetzt.  Sobald eine Sektion aus der
+	 Wertung genommen wird, solle s aber auf jeden Fall gesetzt werden! */
+      var features = features_aus_liste(veranstaltung);
+      features.sektionen_aus_wertung = true;
+      veranstaltung.features = features_zu_liste(features);
+      $scope.busy = true;
+      veranstaltung_speichern($http, veranstaltung.id, veranstaltung).
+	success(function(veranstaltung) {
+	  veranstaltung_zuweisen(veranstaltung);
+	}).
+	error(netzwerkfehler).
+	finally(function() {
+	  delete $scope.busy;
+	});
+    };
+
+    $scope.verwerfen = function() {
+      if ($scope.busy)
+	return;
+      /* FIXME: Wenn Fahrer geladen, neu laden um Versionskonflikte aufzulösen. */
+      veranstaltung_zuweisen(undefined);
+    };
+
+    $scope.keydown = function(event) {
+      if (event.which == 13) {
+	$timeout(function() {
+	  if ($scope.geaendert() && $scope.form.$valid)
+	    $scope.speichern();
+	});
+      } else if (event.which == 27) {
+	$timeout(function() {
+	  if ($scope.geaendert())
+	    $scope.verwerfen();
 	});
       }
-    });
+    };
 
-    angular.forEach(veranstaltung.sektionen_aus_wertung, function(sektionen, index) {
-      var klasse = index + 1;
-      if (sektionen && s[klasse - 1])
-	angular.forEach(sektionen, function(sektionen, index) {
-	  var runde = index + 1;
-	  if (sektionen && s[klasse - 1][runde - 1])
-	    angular.forEach(sektionen, function(sektion) {
-	      s[klasse - 1][runde - 1][sektion - 1] = true;
-	    });
-	});
-    });
-    return s;
-  }
-
-  function sektionen_aus_wertung(sektion_aus_wertung) {
-    var k = [];
-    angular.forEach(sektion_aus_wertung, function(sektionen, index) {
-      var klasse = index + 1;
-      if (sektionen) {
-	var r = [];
-	angular.forEach(sektionen, function(sektionen, index) {
-	  var runde = index + 1;
-	  if (sektionen) {
-	    var s = [];
-	    angular.forEach(sektionen, function(sektion, index) {
-	      if (sektion)
-		s.push(index + 1);
-	    });
-	    if (s.length)
-	      r[runde - 1] = s;
-	  }
-	});
-	if (r.length)
-	  k[klasse - 1] = r;
-      }
-    });
-    return k;
-  }
-
-  $scope.geaendert = function() {
-    return !(angular.equals($scope.sektion_aus_wertung, $scope.sektion_aus_wertung_alt) &&
-	     angular.equals($scope.veranstaltung, $scope.veranstaltung_alt));
-  };
-
-  $scope.speichern = function() {
-    if ($scope.busy)
-      return;
-    /* FIXME: Wenn Klasse schon Starter hat, muss sie weiterhin starten. (Verweis auf Starterliste.) */
-    veranstaltung.sektionen_aus_wertung = sektionen_aus_wertung($scope.sektion_aus_wertung);
-
-    /* Wenn die Daten aus dem Trialtool importiert wurden, ist das Feature
-       sektionen_aus_wertung nicht gesetzt.  Sobald eine Sektion aus der
-       Wertung genommen wird, solle s aber auf jeden Fall gesetzt werden! */
-    var features = features_aus_liste(veranstaltung);
-    features.sektionen_aus_wertung = true;
-    veranstaltung.features = features_zu_liste(features);
-    $scope.busy = true;
-    veranstaltung_speichern($http, veranstaltung.id, veranstaltung).
-      success(function(veranstaltung) {
-	veranstaltung_zuweisen(veranstaltung);
-      }).
-      error(netzwerkfehler).
-      finally(function() {
-	delete $scope.busy;
-      });
-  };
-
-  $scope.verwerfen = function() {
-    if ($scope.busy)
-      return;
-    /* FIXME: Wenn Fahrer geladen, neu laden um Versionskonflikte aufzulösen. */
-    veranstaltung_zuweisen(undefined);
-  };
-
-  $scope.keydown = function(event) {
-    if (event.which == 13) {
-      $timeout(function() {
-	if ($scope.geaendert() && $scope.form.$valid)
-	  $scope.speichern();
-      });
-    } else if (event.which == 27) {
-      $timeout(function() {
-	if ($scope.geaendert())
-	  $scope.verwerfen();
-      });
-    }
-  };
-
-  beim_verlassen_warnen($scope, $scope.geaendert);
-}
+    beim_verlassen_warnen($scope, $scope.geaendert);
+  }];
 
 sektionenController.resolve = {
-  veranstaltung: function($q, $http, $route) {
-    return http_request($q, $http.get('/api/veranstaltung',
-				      {params: $route.current.params}));
-  },
+  veranstaltung: [
+    '$q', '$http', '$route',
+    function($q, $http, $route) {
+      return http_request($q, $http.get('/api/veranstaltung',
+					{params: $route.current.params}));
+    }],
 };
