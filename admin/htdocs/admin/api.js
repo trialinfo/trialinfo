8f3478261085578fbda5ecdab13bdc89df4cf01e
Andreas Gruenbacher <agruenba@redhat.com>
2016-12-15 22:20:58 +0100

    Share function features_from_list

--- admin/htdocs/admin/api.js
+++ admin/htdocs/admin/api.js
@@ -7,18 +7,6 @@ function load_events($scope, $http) {
     });
 }
 
-function features_from_list(event) {
-  var obj = {};
-  angular.forEach(event.features, function(feature) {
-    obj[feature] = true;
-  });
-  obj.rankings = [];
-  for (var n = 1; n <= event.rankings.length; n++)
-    if ('ranking' + n in obj)
-      obj.rankings.push(n);
-  return obj;
-}
-
 function features_to_list(features) {
   var list = [];
   angular.forEach(features, function(value, key) {
