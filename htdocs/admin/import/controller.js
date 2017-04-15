c2f22c97f9a79bb8a7a02b5f1e2863469ba268c9
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2017-04-15 17:56:10 +0200

    Remove TrialTool import stuff from frontend for now
    This can be restored later when fixing TrialTool import and export.

--- htdocs/admin/import/controller.js
+++ htdocs/admin/import/controller.js
@@ -4,7 +4,6 @@ var importController = [
   '$scope', '$http', '$location', '$q', 'events',
   function ($scope, $http, $location, $q, events) {
     $scope.SYNC_SOURCE = SYNC_SOURCE;
-    $scope.TRIALTOOL = TRIALTOOL;
     $scope.events = events;
     $scope.settings = {
       operation: SYNC_SOURCE ? 'import-remote' : 'import-file',
@@ -36,41 +35,6 @@ var importController = [
 	  };
 	  $scope.busy = true;
 	  reader.readAsBinaryString(filename.files[0]);
-	}
-      }
-      if ($scope.settings.format == 'trialtool') {
-	var cfg_datei = document.getElementById('cfg_datei');
-	var dat_datei = document.getElementById('dat_datei');
-	if (cfg_datei && cfg_datei.files[0] && dat_datei && dat_datei.files[0]) {
-	  var cfg_data, dat_data;
-
-	  function datei_gelesen() {
-	    if (cfg_data != null && dat_data != null) {
-	      var data = angular.toJson({ cfg: window.btoa(cfg_data), dat: window.btoa(dat_data) });
-	      $http.post('/api/trialtool/import', data).
-		success(function(result) {
-		  $location.path('/event/' + result.id).replace();
-		}).
-		error(network_error).
-		finally(function() {
-		  $scope.busy = false;
-		});
-	    }
-	  }
-
-	  var cfg_reader = new FileReader();
-	  cfg_reader.onloadend = function(e) {
-	    cfg_data = e.target.result;
-	    datei_gelesen();
-	  };
-	  var dat_reader = new FileReader();
-	  dat_reader.onloadend = function(e) {
-	    dat_data = e.target.result;
-	    datei_gelesen();
-	  };
-	  $scope.busy = true;
-	  cfg_reader.readAsBinaryString(cfg_datei.files[0]);
-	  dat_reader.readAsBinaryString(dat_datei.files[0]);
 	}
       }
     }
