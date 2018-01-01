56c27f73dce7c1f751aa0db2336c1be83931bfed
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2018-01-01 18:06:02 +0100

    Fix syntax error in Perl annual ranking

--- cgi-bin/ergebnisse/jahreswertung-ssi.pl
+++ cgi-bin/ergebnisse/jahreswertung-ssi.pl
@@ -124,7 +124,7 @@ if ($letzte_id) {
     });
     $sth->execute($letzte_id);
     while (my @row = $sth->fetchrow_array) {
-	my $spalte = $result_columns_map->{$row[0]};
+	my $spalte = $result_columns_map->{$row[0]}
 	   or die "Invalid column name\n";
 	if ($spalte eq 'lbl') {
 	   next unless $features->{land} || $features->{bundesland};
