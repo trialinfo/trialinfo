fcb1d1548f0d30be6bf1435b7e4d2bf1787126ac
Andreas Gruenbacher <agruenba@redhat.com>
2019-05-17 00:07:48 +0200

    admin: Show all events during registration (2)
    Hide non-active future events as soon as the registration ends.

--- htdocs/admin/event/riders/controller.js
+++ htdocs/admin/event/riders/controller.js
@@ -403,7 +403,9 @@ var ridersController = [
 
     $scope.future_events = event.future_events.reduce(
       function(future_events, future_event) {
-	if (future_event.active || event.registration_ends != null)
+	if (future_event.active ||
+	    (event.registration_ends != null &&
+	     parse_timestamp(event.registration_ends).getTime() >= Date.now()))
 	  future_events.push(future_event);
 	return future_events;
       }, []);
