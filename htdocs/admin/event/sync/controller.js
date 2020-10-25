3102bc9f4b795da3213184dc88c7893c3ee80544
Andreas Gruenbacher <agruenba@redhat.com>
2020-10-25 22:47:41 +0100

    Rewrite sync code
    When synchronizing an event from host A to host B, also transfer the
    scoring data between A and B: while the non-scoring data is only
    transferred from A to B, the scoring data is transferred in both
    directions.  Each side computes the event results based on the scoring
    data it already has.  The scoring data will eventually converge either
    because the scoring devices manage to talk to host A as well as B or
    through the sync.  At that point, the event results on both sides will
    be identical.
    
    With the old promise-based sync code, even the relatively simple sync
    code was difficult to maintain.  Since browsers now widely support
    async/await, use that instead.  The result is much better now even
    though the code could be cleaned up further.

--- htdocs/admin/event/sync/controller.js
+++ htdocs/admin/event/sync/controller.js
@@ -35,7 +35,9 @@ var syncController = [
       var color;
       if ($scope.running) {
 	if ($scope.source_dump && $scope.target_dump &&
-	    !($scope.patch && $scope.patch.length))
+	    ($scope.target_patch || []).length == 0 &&
+	    Object.keys($scope.source_dump.scoring || {}).length == 0 &&
+	    Object.keys($scope.target_dump.scoring || {}).length == 0)
 	  color = '#27E833';  // "Green"
 	else
 	  color = '#FF7E00';  // "Orange"
@@ -47,13 +49,16 @@ var syncController = [
     };
 
     $scope.start = function() {
+      $scope.target_event_should_exist = false;
+      $scope.source_seq = {};
+      $scope.target_seq = {};
       delete $scope.source_dump;
       delete $scope.target_dump;
-      delete $scope.patch
+      delete $scope.target_patch;
       delete $scope.source_error;
       delete $scope.target_error;
       $scope.running = true;
-      make_target_request(false);
+      sync();
     };
 
     $scope.stop = function() {
@@ -107,20 +112,23 @@ var syncController = [
       return next.promise;
     };
 
-    function url_error(url, fehler) {
+    function url_error(url, error) {
       if (url == '')
-	url = $location.protocol() + '://' + $location.host();
-      if (typeof fehler != 'string') {
-	let response = fehler;
+	url = $location.absUrl().match(/(^.*\/\/[^/]*)/)[1];
+      if (error instanceof Error)
+	return url + ': ' + error.toString();
+
+      if (typeof error != 'string') {
+	let response = error;
 	if (response.status < 0)
-	  fehler = 'Keine Verbindung';
+	  error = 'Keine Verbindung';
 	else {
-	  fehler = response.status;
+	  error = response.status;
 	  if (response.data && response.data.error)
-	    fehler = fehler + ' ' + response.data.error;
+	    error = error + ' ' + response.data.error;
 	}
       }
-      return url + ': ' + fehler;
+      return url + ': ' + error;
     }
 
     const computed_fields = [
@@ -130,13 +138,16 @@ var syncController = [
 
     function reduce_dump(dump) {
       delete dump.event.tag;
-      delete dump.scoring;
+      delete dump.seq;
 
       /*
        * When using mobile scoring, the marks of the zones under scoring and
        * the penalty marks are determined by the scoring data, so leave those
        * and the other computed fields out of the diff.  Otherwise, if the two
        * sides don't have the same scoring data, the diff might not apply.
+       *
+       * We sync the scoring data in both ways, so eventually, the computed
+       * fields on both sides will match as well.
        */
       let scoring_zones = dump.event.scoring_zones;
       if (!scoring_zones.some(function(enabled) { return enabled }))
@@ -171,78 +182,169 @@ var syncController = [
 	  }
 	}
 	rider.marks_per_zone = marks_per_zone;
-	for (let field in computed_fields)
+	for (let field of computed_fields)
 	  delete rider[field];
+      }
+      return dump;
+    }
+
+    function dump_without_scoring(dump) {
+      dump = Object.assign({}, dump);
+      delete dump.scoring;
+      return dump;
+    }
+
+    function update_seq(seq, scoring) {
+      for (device_tag in scoring) {
+	let items = scoring[device_tag];
+	let max_seq = items[items.length - 1].seq;
+	if (!(seq[device_tag] >= max_seq))
+	  seq[device_tag] = max_seq;
       }
     }
 
-    function sync() {
-      reduce_dump($scope.source_dump);
-      reduce_dump($scope.target_dump);
-      $scope.patch = json_diff($scope.target_dump, $scope.source_dump);
-      if ($scope.patch.length) {
-	// $scope.patch.unshift({op: 'test', path: '/event/fahrer_version',
-	//			    value: data0.event.fahrer_version });
-	//$scope.patch.unshift({op: 'test', path: '/event/version',
-	//		      value: $scope.target_dump.event.version });
-	//$scope.patch.unshift({op: 'test', path: '/format',
-	//		      value: $scope.target_dump.format });
+    function reduce_scoring(scoring, seq) {
+      let reduced_scoring = {};
+      for (let device_tag in scoring) {
+	let items = scoring[device_tag];
+	let n;
+	for (n = 0; n < items.length; n++) {
+	  if (!(seq[device_tag] >= items[n].seq))
+	    break;
+	}
+	if (n != items.length) {
+	  if (n != 0)
+	    items = items.splice(n);
+	  reduced_scoring[device_tag] = items;
+	}
+      }
+      return reduced_scoring;
+    }
 
+    function source_patch_request() {
+      let target_scoring =
+	reduce_scoring($scope.target_dump.scoring, $scope.source_seq);
+      $scope.target_dump.scoring = target_scoring;
+      if (Object.keys(target_scoring).length) {
+	let seq = Object.assign({}, $scope.source_seq);
+	update_seq(seq, $scope.source_dump.scoring);
+	var params = {
+	  seq: encodeURIComponent(JSON.stringify(seq))
+	};
+	let cancel = $q.defer();
+	let request =
+	  $http.post('/api/event/' + $scope.from_tag + '/patch',
+		     {patch: [],
+		      scoring: target_scoring},
+		     {params: params,
+		      timeout: cancel.promise});
+	return make_request('patch source request', request, cancel)
+	  .then(function(response) {
+	    update_seq($scope.source_seq, target_scoring);
+	    $scope.target_dump.scoring = {};
+	    /* response.scoring could end up non-empty here, but that doesn't
+	     * matter because we'll pick up any additional scoring items in the
+	     * next iteration.  */
+	  })
+	  .catch(function(response) {
+	    $scope.source_error = url_error('', response);
+	    if (response.status == 404 || response.status == 409)
+	      $scope.stop();
+	  });
+      } else {
+	let pass = $q.defer();
+	pass.resolve();
+	return pass.promise;
+      }
+    }
+
+    function target_patch_request() {
+      let source_scoring =
+	reduce_scoring($scope.source_dump.scoring, $scope.target_seq);
+      $scope.source_dump.scoring = source_scoring;
+      let source_dump = dump_without_scoring($scope.source_dump);
+      let target_dump = dump_without_scoring($scope.target_dump);
+      $scope.target_patch = json_diff(target_dump, source_dump);
+
+      /*
+      let scoring_active = $scope.source_dump.event.scoring_zones
+        .some(function(enabled) { return enabled; });
+      */
+
+      if ($scope.target_patch.length || Object.keys(source_scoring).length /*|| scoring_active*/) {
+	let seq = Object.assign({}, $scope.target_seq);
+	update_seq(seq, $scope.target_dump.scoring);
+	var params = {
+          seq: encodeURIComponent(JSON.stringify(seq))
+	};
 	var cancel = $q.defer();
 	var request =
 	  $http.post($scope.url + '/api/event/' + $scope.to_tag + '/patch',
-		     {patch: $scope.patch},
+		     {patch: $scope.target_patch,
+		      scoring: source_scoring},
 		     {restart: restart_param(),
+		      params: params,
 		      timeout: cancel.promise,
 		      withCredentials: true});
-	return make_request('patch request', request, cancel)
-	  .then(function() {
-	    $scope.target_dump = $scope.source_dump;
-	    delete $scope.patch;
+	return make_request('patch target request', request, cancel)
+	  .then(function(response) {
+	    update_seq($scope.target_seq, source_scoring);
+	    $scope.target_dump = Object.assign({}, $scope.source_dump);
+	    $scope.target_dump.scoring = response.scoring;
+	    $scope.source_dump.scoring = {};
+	    delete $scope.target_patch;
 	    delete $scope.target_error;
 	  })
 	  .catch(function(response) {
 	    $scope.target_error = url_error($scope.url, response);
-	    if (response.status == 409)
+	    if (response.status == 404 || response.status == 409)
 	      $scope.stop();
 	  });
       } else {
-	// Ein Versionscheck am Target wäre vielleicht nicht blöd ...
-	var deferred = $q.defer();
-	deferred.resolve();
-	return deferred.promise;
+	let pass = $q.defer();
+	pass.resolve();
+	return pass.promise;
       }
     }
 
-    function make_source_request() {
+    function source_dump_request() {
       delete $scope.source_dump;
       var cancel = $q.defer();
-      var source_request =
-	$http.get('/api/event/' + $scope.from_tag + '/dump',
-		  {timeout: cancel.promise});
-      return make_request('source request', source_request, cancel)
+      var params = {
+	seq: encodeURIComponent(JSON.stringify($scope.target_seq))
+      };
+      var request =
+	$http.get('/api/event/' + $scope.from_tag + '/dump', {
+	  params: params,
+	  timeout: cancel.promise
+	});
+      return make_request('source request', request, cancel)
 	.then(function(response) {
 	  let dump = response.data;
-	  $scope.source_dump = dump;
+	  if (!dump.format) {
+	    $scope.source_error = 'Fehlerhafte Antwort vom Server';
+	    return $q.reject();
+	  }
+	  $scope.source_seq = dump.seq || {};
+	  $scope.source_dump = reduce_dump(dump);
 	  $scope.title = dump.event.title;
 	  delete $scope.source_error;
-	  sync()
-	    .then(function() {
-	      if ($scope.running)
-		later('source timeout', make_source_request, $scope.timeout);
-	    });
 	})
 	.catch(function(response) {
 	  $scope.source_error = url_error('', response);
-	  later('source timeout', make_source_request, $scope.timeout);
+	  return $q.reject();
 	});
     }
 
-    function make_target_request(prevent_import) {
+    function target_dump_request() {
       delete $scope.target_dump;
       var cancel = $q.defer();
+      var params = {
+	seq: encodeURIComponent(JSON.stringify($scope.source_seq))
+      };
       var target_request =
 	$http.get($scope.url + '/api/event/' + $scope.to_tag + '/dump', {
+	  params: params,
 	  restart: restart_param(),
 	  timeout: cancel.promise,
 	  withCredentials: true
@@ -250,56 +352,102 @@ var syncController = [
       return make_request('target request', target_request, cancel)
 	.then(function(response) {
 	  let dump = response.data;
-	  if (dump.format) {
-	    delete $scope.target_error;
-	    $scope.target_dump = dump;
-	    make_source_request();
-	  } else {
+	  if (!dump.format) {
 	    $scope.target_error = 'Fehlerhafte Antwort vom Server';
-	    $scope.stop();
+	    return $q.reject();
 	  }
+	  delete $scope.target_error;
+	  $scope.target_seq = dump.seq || {};
+	  $scope.target_dump = reduce_dump(dump);
+	  $scope.target_event_should_exist = true;
 	})
 	.catch(function(response) {
-	  if (prevent_import) {
-	    $scope.target_error = url_error($scope.url, 'Veranstaltung ' + $scope.to_tag + ' existiert nicht');
-	    $scope.stop();
-	    return;
-	  }
-
 	  if (response.status == 404) {
-	    var cancel = $q.defer();
-	    var export_request =
-	      $http.get('/api/event/' + $scope.from_tag + '/export',
-			{timeout: cancel.promise,
-			 responseType: 'arraybuffer'});
-	    make_request('export request', export_request, cancel)
-	      .then(function(response) {
-		var enc = window.btoa(String.fromCharCode.apply(null, new Uint8Array(response.data)));
-		var cancel = $q.defer();
-		var import_request =
-		  $http.post($scope.url + '/api/event/import',
-			     {data: enc},
-			     {restart: restart_param(),
-			      timeout: cancel.promise,
-			      withCredentials: true});
-		  make_request('import request', import_request, cancel)
-		    .then(function() {
-		      make_target_request(true);
-		    })
-		    .catch(function(response) {
-		      $scope.target_error = url_error($scope.url, response);
-		      $scope.stop();
-		    });
-	      })
-	      .catch(function() {
-		$scope.source_error = url_error('', 'Export fehlgeschlagen');
-		$scope.stop();
-	      });
+	    if ($scope.target_event_should_exist) {
+	      $scope.target_error = url_error($scope.url, new Error('Veranstaltung ' + $scope.to_tag + ' existiert nicht'));
+	      return $q.reject();
+	    }
+	    let pass = $q.defer();
+	    pass.resolve();
+	    return pass.promise;
 	  } else {
-	    $scope.target_error = url_error($scope.url, response);
-	    later('target timeout', make_target_request, $scope.timeout);
+	    $scope.target_error = url_error('', response);
+	    return $q.reject();
 	  }
 	});
+    }
+
+    function source_export_request() {
+      var cancel = $q.defer();
+      var export_request =
+	$http.get('/api/event/' + $scope.from_tag + '/export',
+		  {timeout: cancel.promise,
+		   responseType: 'arraybuffer'});
+      return make_request('export request', export_request, cancel)
+	.then(function(response) {
+	  /* FIXME: We could extract the source sequence numbers and
+	     update $scope.source_seq here to save some bandwidth here,
+	     but the data is compressed. */
+	  return new Uint8Array(response.data);
+	})
+	.catch(function() {
+	  $scope.source_error = url_error('', new Error('Export fehlgeschlagen'));
+	  return $q.reject();
+	});
+    }
+
+    function target_import_request(data) {
+      var enc = window.btoa(String.fromCharCode.apply(null, data));
+      var cancel = $q.defer();
+      var import_request =
+	$http.post($scope.url + '/api/event/import',
+		   {data: enc},
+		   {restart: restart_param(),
+		    timeout: cancel.promise,
+		    withCredentials: true});
+	return make_request('import request', import_request, cancel)
+	  .then(function() {
+	    $scope.target_event_should_exist = true;
+	    /* FIXME: We could extract the source sequence numbers
+	       and update $scope.target_seq to save some bandwidth
+	       here, but the data is compressed.  */
+	  })
+	  .catch(function(response) {
+	    $scope.target_error = url_error($scope.url, response);
+	    return $q.reject();
+	  });
+    }
+
+    async function sync_step() {
+      await source_dump_request();
+      if (!$scope.target_dump) {
+        await target_dump_request();
+	if (!$scope.target_dump) {
+	  if ($scope.target_event_should_exist) {
+	    let error = new Error('Veranstaltung ' + $scope.to_tag + ' existiert nicht');
+	    $scope.target_error = url_error($scope.url, error);
+	    throw error;
+	  }
+	  let data = await source_export_request();
+	  await target_import_request(data);
+	  await target_dump_request();
+	}
+      }
+      await source_patch_request();
+      await target_patch_request();
+      /* Re-patch the source in case we have additional scoring data now. */
+      await source_patch_request();
+    }
+
+    async function sync() {
+      if (!$scope.running)
+	return;
+      try {
+	await sync_step();
+	later('target timeout', sync, $scope.timeout);
+      } catch (error) {
+	$scope.stop();
+      }
     }
 
     $scope.$on('sync', function(event, args) {
