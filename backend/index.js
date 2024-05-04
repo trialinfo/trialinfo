d947e99e51ca4712e29ef05513761a5792008697
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2024-05-05 00:14:08 +0200

    Fix skipped zone handling in event results
    The wrong variable is being passed down to rider_result() ->
    result_marks_per_zone() in get_event_results().  I don't know if this
    case has ever triggered in a real event.

--- backend/index.js
+++ backend/index.js
@@ -2542,7 +2542,7 @@ async function get_event_results(connection, id) {
 	rider[field] = cached_rider[field];
       if ((last_event.classes[cached_rider.class - 1] || {}).non_competing)
 	rider.non_competing = true;
-      let result = rider_result(cached_rider, events[0], ranking_class);
+      let result = rider_result(cached_rider, events[0], rc);
       for (let field of ['decisive_round'])
 	result[field] = cached_rider[field];
       if (cached_rider.unfinished_zones && !cached_rider.failure)
